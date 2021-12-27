31a6315f34ff1bcc227874312d02dabdb49fae78
Pablo Orviz <orviz@ifca.unican.es>
2021-12-27 13:01:15 +0100

    Fix: add absolute path to spec

--- generate-api-code.sh
+++ generate-api-code.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
-    -i https://github.com/orviz/openapi4pm/raw/main/openapi.yaml \
+    -i /local/openapi.yaml \
     -g python-flask \
     -o /local/flask_code
